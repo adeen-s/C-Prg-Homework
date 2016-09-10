b57128e12d32208b86ca6a3df78c8bbe6cc125ed
adeen-s <adeens357@gmail.com>
2016-09-10 17:14:27 +0530

    Check for Prime Number

--- NM/isPrime.c
+++ NM/isPrime.c
@@ -1,2 +1,22 @@
 #include<stdio.h>
-int main()
+int main() {
+	int i,num,flag=0;
+	system("cls || clear");
+	printf("\nEnter a number\n");
+	scanf("%d",&num);
+	if(num==1) {
+		printf("\n 1 is neither prime nor composite \n");
+		return 0;
+	}
+	for(i=2;i<=num/2;i++) {
+		if(num%i==0) {
+			printf("\n %d is not prime \n",num);
+			flag=1;
+			break;
+		}
+	}
+	if(!flag)
+	printf("\n %d is Prime\n ",num);
+	getchar();
+	return 0;
+}	
